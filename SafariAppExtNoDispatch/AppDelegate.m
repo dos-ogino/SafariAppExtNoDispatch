7b7ef45c51dc935123c9daf5a1be4c2b2f77cb09
ogino <ogino_takayuki@dos-osaka.co.jp>
2019-07-16 10:51:05 +0900

    Initial Commit

---
+++ SafariAppExtNoDispatch/AppDelegate.m
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.m
+//  
+//  
+//  Created by ogino on 2019/07/16
+//  Copyright © 2019 SS1. All rights reserved.
+//
+// 更新履歴
+// *********************************************************************************************************************************
+// | 日付        | 氏名       | 修正Ver    | キーワード           | 対応内容                                                            |
+// *********************************************************************************************************************************
+// | 2019/07/16 | T.Ogino   | xx.x.x     |                    |                                                                   |
+// *********************************************************************************************************************************
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
